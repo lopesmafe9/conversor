1b1772266e3c2207a22b869c46f6a90e570d25d9
mariafernanda <mariafernandamf09mf@gmail.com>
2025-09-25 11:20:08 -0300

    adicionando função de conversão

--- moeda.py
+++ moeda.py
@@ -7,3 +7,11 @@ taxas = {
     ("EUR", "USD"): 1.17
 }
 
+def converter_moeda (valor, de, para){
+    if de == para:
+        return valor
+    if (de, para) in taxas:
+        return valor * taxas [(de, para)]
+    else:
+        return None
+}
