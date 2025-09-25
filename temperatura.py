114662160be3e6802903671a2b21836b57f77d31
mariafernanda <mariafernandamf09mf@gmail.com>
2025-09-25 10:56:48 -0300

    parte de conversão adicionada

--- temperatura.py
+++ temperatura.py
@@ -8,3 +8,13 @@ def converter_temperatura (valor, de, para):
         valor_k = valor
     else:
         return None
+
+#converte de kelvin para o destino
+if para == "C":
+    return valor_k - 274.15
+elif para == "F":
+    return (valor_k - 273.15) * 9/5 + 32
+elif para == "K":
+    return valor_k
+else:
+    return None
